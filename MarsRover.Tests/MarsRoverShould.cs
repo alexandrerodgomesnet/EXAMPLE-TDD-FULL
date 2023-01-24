2e1389428e38ec4d9117157575836a46a1965888
alexandrerodgomesnet <alexandrerodgomes@hotmail.com>
2023-01-24 12:48:15 -0300

    Change Test MoveForward.

--- MarsRover.Tests/MarsRoverShould.cs
+++ MarsRover.Tests/MarsRoverShould.cs
@@ -17,16 +17,16 @@ public class MarsRoverShould
     }
 
     [Theory]
-    [InlineData("0:0:N", "0:1:N")]
-    [InlineData("1:1:N", "1:2:N")]
-    [InlineData("1:1:W", "0:1:W")]
-    [InlineData("1:1:E", "2:1:E")]
-    [InlineData("1:1:S", "1:0:S")]
-    public void MoveForward(string initialState, string expectedFinalState)
+    [InlineData("0:0:N", "M", "0:1:N")]
+    [InlineData("1:1:N", "M", "1:2:N")]
+    [InlineData("1:1:W", "M", "0:1:W")]
+    [InlineData("1:1:E", "M", "2:1:E")]
+    [InlineData("1:1:S", "M", "1:0:S")]
+    public void MoveForward(string initialState, string command, string expectedFinalState)
     {
         var marsRover = new MarsRover(initialState);
 
-        var result = marsRover.Execute("M");
+        var result = marsRover.Execute(command);
 
         result.Should().Be(expectedFinalState);
     }
