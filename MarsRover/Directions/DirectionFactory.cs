15153275a233529bcf1b5af05ef038116406b0a5
alexandrerodgomesnet <alexandrerodgomes@hotmail.com>
2023-01-24 12:40:03 -0300

    Refactor DirectionFactory;

--- MarsRover/Directions/DirectionFactory.cs
+++ MarsRover/Directions/DirectionFactory.cs
@@ -2,20 +2,12 @@ namespace MarsRover.Directions;
 
 public class DirectionFactory
 {
-    public static IDirection CreateFrom(string direction)
+    public static IDirection CreateFrom(string direction) => direction switch
     {
-        switch (direction)
-        {
-            case "N":
-                return new North();
-            case "W":
-                return new West();
-            case "E":
-                return new East();
-            case "S":
-                return new South();
-            default:
-                throw new NotSupportedException($"Direction {direction} is not supported.");
-        }
-    }
+        "N" => new North(),
+        "W" => new West(),
+        "E" => new East(),
+        "S" => new South(),
+        _ => throw new NotSupportedException($"Direction {direction} is not supported.")
+    };
 }
