8fb4fce212cd51a78d011fe28652e7d3f0a40476
alexandrerodgomesnet <alexandrerodgomes@hotmail.com>
2023-01-24 12:26:58 -0300

    Create DirectionFactory.

---
+++ MarsRover/DirectionFactory.cs
@@ -0,0 +1,19 @@
+namespace MarsRover;
+
+public class DirectionFactory
+{
+    public static IDirection CreateDirectionFrom(string direction)
+    {
+        switch (direction)
+        {
+            case "N":
+                return new North();
+            case "W":
+                return new West();
+            case "E":
+                return new East();
+            default:
+                throw new NotSupportedException($"Direction {direction} is not supported.");
+        }
+    }
+}
--- MarsRover/MarsRover.cs
+++ MarsRover/MarsRover.cs
@@ -20,25 +20,10 @@ public class MarsRover
 
         if (commands == "M")
         {
-            _direction = CreateDirectionFrom(directionStringCommand);
+            _direction = DirectionFactory.CreateDirectionFrom(directionStringCommand);
             _position = _direction.MoveForward(_position);
         }
 
         return $"{_position.X}:{_position.Y}:{directionStringCommand}";
-    }
-
-    private IDirection CreateDirectionFrom(string direction)
-    {
-        switch (direction)
-        {
-            case "N":
-                return new North();
-            case "W":
-                return new West();
-            case "E":
-                return new East();
-            default:
-                throw new NotSupportedException($"Direction {direction} is not supported.");
-        }
     }
 }
