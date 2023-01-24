d5846fdc5a5a6128c1a2a763621a33860becbe4b
alexandrerodgomesnet <alexandrerodgomes@hotmail.com>
2023-01-24 12:50:12 -0300

    Refactor Tests.

--- MarsRover.Tests/MarsRoverShould.cs
+++ MarsRover.Tests/MarsRoverShould.cs
@@ -5,24 +5,14 @@ namespace MarsRover.Tests;
 public class MarsRoverShould
 {
     [Theory]
-    [InlineData("0:0:N")]
-    [InlineData("2:1:W")]
-    public void PrintInitialState(string initialState)
-    {        
-        var marsRover = new MarsRover(initialState);
-
-        var result = marsRover.Execute("");
-
-        result.Should().Be(initialState);
-    }
-
-    [Theory]
+    [InlineData("0:0:N", "", "0:0:N")]
+    [InlineData("2:1:W", "", "2:1:W")]
     [InlineData("0:0:N", "M", "0:1:N")]
     [InlineData("1:1:N", "M", "1:2:N")]
     [InlineData("1:1:W", "M", "0:1:W")]
     [InlineData("1:1:E", "M", "2:1:E")]
     [InlineData("1:1:S", "M", "1:0:S")]
-    public void MoveForward(string initialState, string command, string expectedFinalState)
+    public void ExecuteCommands(string initialState, string command, string expectedFinalState)
     {
         var marsRover = new MarsRover(initialState);
 
