6dc55cd114e21eea3c3ceb055987cd6ec9fb3c17
alexandrerodgomesnet <alexandrerodgomes@hotmail.com>
2023-01-24 11:06:44 -0300

    print another position

--- MarsRover.Tests/MarsRoverShould.cs
+++ MarsRover.Tests/MarsRoverShould.cs
@@ -5,12 +5,22 @@ namespace MarsRover.Tests;
 public class MarsRoverShould
 {
     [Fact]
-    public void PrintInitialState()
+    public void PrintInitialStateFromOnePosition()
     {        
         var marsRover = new MarsRover("0:0:N");
 
         var result = marsRover.Execute("");
 
         result.Should().Be("0:0:N");
+    }
+
+    [Fact]
+    public void PrintInitialStateFromAnotherPosition()
+    {        
+        var marsRover = new MarsRover("2:1:W");
+
+        var result = marsRover.Execute("");
+
+        result.Should().Be("2:1:W");
     }
 }
--- MarsRover/MarsRover.cs
+++ MarsRover/MarsRover.cs
@@ -2,13 +2,15 @@ namespace MarsRover;
 
 public class MarsRover
 {
+    private readonly string _initialState;
+
     public MarsRover(string initialState)
     {
-        
+        _initialState = initialState;
     }
 
     public string Execute(string commands)
     {
-        return "0:0:N";
+        return _initialState;
     }
 }
