cfabd3526f334a6c57d0b47d25fba6fc41595c1b
alexandrerodgomesnet <alexandrerodgomes@hotmail.com>
2023-01-24 16:18:18 -0300

    example command.

--- MarsRover/Program.cs
+++ MarsRover/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+namespace MarsRover;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //example command
+        //.\MarsRover\bin\Debug\net7.0\MarsRover.exe 0:0:N MMMMMMLLLLRRRRRMMMML
+        Console.WriteLine(new MarsRover(args[0]).Execute(args[1]));
+    }
+}
